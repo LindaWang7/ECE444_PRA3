e487ce6a3de618b812a24022533b10992f8a6e51
Linda Wang <48342363+LindaWang7@users.noreply.github.com>
2024-10-03 00:29:09 -0400

    added style.css

--- project/app.py
+++ project/app.py
@@ -1,6 +1,5 @@
 import sqlite3
-from flask import Flask, g, render_template, request, session, flash, redirect, url_for
-
+from flask import Flask, g, render_template, request, session, flash, redirect, url_for, abort
 # configuration
 # configuration
 DATABASE = "flaskr.db"
@@ -14,6 +13,19 @@ app = Flask(__name__)
 # load the config
 app.config.from_object(__name__)
 
+@app.route('/add', methods=['POST'])
+def add_entry():
+    """Add new post to database."""
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute(
+        'insert into entries (title, text) values (?, ?)',
+        [request.form['title'], request.form['text']]
+    )
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('index'))
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     """User login/authentication/session management."""
